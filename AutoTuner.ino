b5948d89a77386087a7becd2fd91469a6deaf9d6
mbk6 <54648422+mbk6@users.noreply.github.com>
2020-04-29 13:44:30 -0500

    Update AutoTuner.ino

--- AutoTuner.ino
+++ AutoTuner.ino
@@ -1,125 +1,125 @@
-//This takes an analog signal from pin A0, finds the most dominant frequency, and generates a smoothed pitch output.
+//AUTOTUNER BY Michael Korenchan, JT Kirages, Adam Aaronson
+//This program takes an analog signal from pin A0, uses the included arduinoFFT library to find the most dominant frequency, and generates a smoothed pitch output and intonation.
 //The raw peak frequency, smoothed pitch, note name, and intonation are printed to the serial plotter
-//Michael Korenchan, JT Kirages, Adam Aaronson
 
 #include "arduinoFFT.h"
 #include <math.h>
 #include <Stepper.h>
-#include <ArduinoQueue.h>
+#include "HashMap.h"
 
-//QUEUE stuff
-ArduinoQueue<double> q;
-int q_size = 20;
-double average = 0;
-
-//EMA Stuff
+//EMA Stuff - for filtering
 float EMA_a = 0.2;      //initialization of EMA alpha
 int EMA_S = 0;  
 
 using namespace std;
 
+//Note recognition
 #define A_HERTZ 440
 #define SEMITONE_RATIO pow(2, 1/12.)
- 
-#define SAMPLES 128
-#define SAMPLING_FREQUENCY 1400 
+String noteArray[] = {
+    "A","Bb","B","C",
+    "Db","D","Eb","E",
+    "F","Gb","G","Ab"
+};
 
+HashMap<int, String> noteMap[12];
+noteMap[0](0, "A");
+noteMap[1](1, "Bb");
+noteMap[2](2, "B");
+noteMap[3](3, "C");
+noteMap[4](4, "Db");
+noteMap[5](5, "D");
+noteMap[6](6, "Eb");
+noteMap[7](7, "E");
+noteMap[8](8, "F");
+noteMap[9](9, "Gb");
+noteMap[10](10, "G");
+noteMap[11](11, "Ab");
+
+
+//FFT
+#define SAMPLES 128
+#define SAMPLING_FREQUENCY 1400
+arduinoFFT FFT = arduinoFFT();
+unsigned int sampling_period_us;
+unsigned long microseconds;
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+
+//Motor Control
 #define motorPin1  8     // IN1 on the ULN2003 driver 1
 #define motorPin2  9     // IN2 on the ULN2003 driver 1
 #define motorPin3  10    // IN3 on the ULN2003 driver 1
 #define motorPin4  11    // IN4 on the ULN2003 driver 1
+const int stepsPerRevolution = 2048;
+Stepper motor = Stepper(stepsPerRevolution, 8, 10, 9, 11);
  
-arduinoFFT FFT = arduinoFFT();
-unsigned int sampling_period_us;
-unsigned long microseconds;
 unsigned int aReadPin = A0;
-unsigned int val;
+
+//Smoothing Parameters
 double pitch = -1;
 double target = 0;
 double smooth_vel = 0;
 double smooth_accel = 0;
 double peak_vel = 0;
 double last_peak = -1;
-double minPeak = 100;
-double maxPeak = 650;
-const int stepsPerRevolution = 2048;
-Stepper motor = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 
- 
-double vReal[SAMPLES];
-double vImag[SAMPLES];
 
-String noteArray[] = {
-    "A","Bb","B","C",
-    "Db","D","Eb","E",
-    "F","Gb","G","Ab"};
-
- 
+//Initialize serial output, motor speed, signal variables
 void setup() {
     Serial.begin(9600);
     sampling_period_us = round(1000000*(1.0/SAMPLING_FREQUENCY));
     motor.setSpeed(5);
-
     pinMode(A0, INPUT);
     EMA_S = analogRead(aReadPin);
 }
-
-bool inRange(double peak) {
-  return (peak > minPeak) && (peak < maxPeak);
-}
  
 void loop() {
-   
+    /*
+     * FFT CODE BASED ON: https://www.norwegiancreations.com/2017/08/what-is-fft-and-how-can-you-implement-it-on-an-arduino/
+     */
     //SAMPLING
     for(int i=0; i<SAMPLES; i++)
     {
         microseconds = micros();
-
         int raw = analogRead(aReadPin);
-        EMA_S = (EMA_a*raw) + ((1-EMA_a)*EMA_S);
+        EMA_S = (EMA_a*raw) + ((1-EMA_a)*EMA_S); //Low-pass filtering
         vReal[i] = EMA_S;
         vImag[i] = 0;
         while(micros() < (microseconds + sampling_period_us)){
         }
     }
-    //FFT code based on https://www.norwegiancreations.com/2017/08/what-is-fft-and-how-can-you-implement-it-on-an-arduino/
     FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
     FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
     FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
     double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
-    
-    
+    /*
+     * END CITATION
+     */
+
+    //Readjust peak - for some reason the arduinoFFT library returns a frequency a little too large
     peak = (peak - 0.1335) / 1.018;
 
-
-//    if(q.item_count() >= q_size) {
-//      if(abs(average - peak) < 5) {
-//        target = peak;
-//      }
-//      average -= q.dequeue() / q_size;
-//      average += peak / q_size;
-//      q.enqueue(peak);
-//    }
-//    else {
-//      q.enqueue(peak);
-//      average += peak / q_size;
-//    }
-
-
-
-
-    
+    //Initialize previous peak record
     if(last_peak < 0) {
       last_peak = peak;
     }
+
+    //Use change in peak to detect unwanted spikes
     peak_vel = peak - last_peak;
     last_peak = peak;
+
+    //Update pitch if negative
     if(pitch < 0) {
       pitch = peak;
     }
+    
+    //If peak changes too quickly, don't update the target
     if(abs(peak_vel) < 10) {
       target = peak;
     }
+    
+    //Approach the target smoothly
     smooth_accel = -0.8*smooth_vel + 0.05*(target - pitch);
     smooth_vel += smooth_accel;
     pitch += smooth_vel;
@@ -127,19 +127,23 @@ void loop() {
     double intonationLevel = getIntonation(pitch);
 
    
-    //Serial.print(peak);
-    Serial.print(" ");
+    //Print smoothed pitch
     Serial.print(pitch);
     Serial.print(" ");
+    //Print scaled intonation
     Serial.print(100*intonationLevel);
     Serial.print(" ");
+    //Print perfect pitch line for reference
     Serial.print(0);
     Serial.print(" ");
+    //Print note name
     Serial.println(getNoteName(pitch));
  
-
+    //Update motor position
     motor.step(stepsPerRevolution * intonationLevel * 0.01);
 }
+
+
 /**
  * Returns a double between -1 and 1
  * Uses A440 12-TET tuning
@@ -164,5 +168,6 @@ double getIntonation(double inputHertz) {
 String getNoteName(double inputHertz) {
     int semitones = round(log(inputHertz / A_HERTZ) / log(SEMITONE_RATIO));
     int noteValue = ((semitones % 12) + 12) % 12;
-    return noteArray[noteValue];
+    //return noteArray[noteValue];
+    return noteMap.getValueOf(noteValue);
 }
